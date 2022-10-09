6dc01937342d772cc0dc3cd1a38179cc091f85a0
James <jameshilditch09@gmail.com>
2022-10-09 20:04:55 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>OMSI 2 Mods by MIO</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
